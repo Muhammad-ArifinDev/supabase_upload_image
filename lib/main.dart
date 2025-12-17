f3bed32022f4a558bf5041c101f69a33e25ae1a4
Muhammad-ArifinDev <ipin93936@gmail.com>
2025-12-17 20:02:07 +0700

    menambahkan Inisialisasi Supabase pada main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:supabase/home_page.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
-  runApp(const MyApp());
+const supabaseUrl = 'https://pcbruukijrkuumzqggma.supabase.co';
+const supabasekey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBjYnJ1dWtpanJrdXVtenFnZ21hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5Njg4MjgsImV4cCI6MjA4MTU0NDgyOH0.-_ujlaTtDR4n--6q5wU3m3mKaeXT_PAvrrV-QqPYXSo';
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Supabase.initialize(url: supabaseUrl, anonKey: supabasekey);
+runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Supabase Upload Image',
-      home: MyApp(),
+      home: MyHomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
